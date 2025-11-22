884bfb1e49d49471ea4d8453a6236d3498a5b2d3
MigueM <juegospc789@hotmail.com>
2025-11-22 10:33:06 -0600

    config :  configuracion e instalacion de librerias

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    entry: './src/main.ts',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.vue', '.json'],
+    },
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
